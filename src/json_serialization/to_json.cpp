52a81f19fcbd0d9108d73b4dc3fe20ab75ea675c
fengzhaoyu <921797341@qq.com>
2023-05-16 02:04:12 +0000

    idHolder add derive-type

--- src/json_serialization/to_json.cpp
+++ src/json_serialization/to_json.cpp
@@ -1,30 +1,3 @@
-/************************************************************************************
- *                                                                                   *
- *   Copyright (c) 2014 - 2018 Axel Menzel <info@rttr.org>                           *
- *                                                                                   *
- *   This file is part of RTTR (Run Time Type Reflection)                            *
- *   License: MIT License                                                            *
- *                                                                                   *
- *   Permission is hereby granted, free of charge, to any person obtaining           *
- *   a copy of this software and associated documentation files (the "Software"),    *
- *   to deal in the Software without restriction, including without limitation       *
- *   the rights to use, copy, modify, merge, publish, distribute, sublicense,        *
- *   and/or sell copies of the Software, and to permit persons to whom the           *
- *   Software is furnished to do so, subject to the following conditions:            *
- *                                                                                   *
- *   The above copyright notice and this permission notice shall be included in      *
- *   all copies or substantial portions of the Software.                             *
- *                                                                                   *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR      *
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
- *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE     *
- *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER          *
- *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,   *
- *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE   *
- *   SOFTWARE.                                                                       *
- *                                                                                   *
- *************************************************************************************/
-
 #include "common.h"
 #include "myrttr/variant.h"
 #include "myrttr/type"
@@ -100,10 +73,10 @@ void to_json_recursively(const instance& obj, PrettyWriter<StringBuffer>& writer
 
 ID_TYPE write_variant(const variant& var, PrettyWriter<StringBuffer>& writer);
 
-void write_IdHolder(const ID_TYPE cid, PrettyWriter<StringBuffer>& writer)
+void write_IdHolder(io::IdHolder holder, PrettyWriter<StringBuffer>& writer)
 {
-  io::IdHolder id_holder{cid};
-  to_json_recursively(id_holder, writer);
+  // io::IdHolder id_holder{cid};
+  to_json_recursively(holder, writer);
 }
 
 bool write_atomic_types_to_json(const type& t, const variant& var, PrettyWriter<StringBuffer>& writer)
@@ -177,7 +150,11 @@ static void write_array(const variant_sequential_view& view, PrettyWriter<String
       {
         // TODO(): 处理id
         auto id = to_json_recursively(wrapped_var);
-        write_IdHolder(id, writer);
+        write_IdHolder(
+            {
+                id,
+            },
+            writer);
       }
     }
   }
